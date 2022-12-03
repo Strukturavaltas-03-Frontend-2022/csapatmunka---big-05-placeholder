1fccd8d25309ca648ee36754997bcbbc585ed70b
rekahamburger <rekahamburger@gmail.com>
2022-12-03 14:32:44 +0100

    service.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EditBillComponent } from './page/edit-bill/edit-bill.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditBillComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/model/bill.ts
+++ src/app/model/bill.ts
@@ -1,2 +1,10 @@
+import { PatternValidator } from "@angular/forms";
+
 export class Bill {
+
+  [x: string]: any;
+    id: number = 0;
+    orderID: number =1;
+    amount: number = 0;
+    status: 'new' | 'paid' = 'new';
 }
